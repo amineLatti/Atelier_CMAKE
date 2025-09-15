98e7b2f4494856cd7614f83a8bd430d387e9084c
Amine LATTI <amine.latti1@gmail.com>
2025-09-15 15:10:31 +0200

    feat: added feature square to the lib

--- src/main.c
+++ src/main.c
@@ -4,29 +4,36 @@
 #include "./../include/calculator.h"
 
 int main(int argc, char *argv[]){
-	if (argc == 4)
-	{
 	char* op = argv[1];
-	char* a = argv[2];
-	char* b = argv[3];
 	double r = 0;
-	if (strcmp(op, "add") == 0) {
-		r = _add(atof(a), atof(b));
+
+	if (argc == 4 && (strcmp(op, "add") == 0 || strcmp(op, "sub") == 0 || 
+		strcmp(op, "mul") == 0 || strcmp(op, "div") == 0))
+	{
+		char* a = argv[2];
+		char* b = argv[3];
+		
+		if (strcmp(op, "add") == 0) {
+			r = _add(atof(a), atof(b));
+		}
+		else if (strcmp(op, "sub") == 0) {
+			r = _sub(atof(a), atof(b));
+		}
+		else if (strcmp(op, "mul") == 0) {
+			r = _mul(atof(a), atof(b));
+		}
+		else if (strcmp(op, "div") == 0) {
+			r = _div(atof(a), atof(b));
+		}
 		printf("%lf",r);
 	}
-	else if (strcmp(op, "sub") == 0) {
-		r = _sub(atof(a), atof(b));
+	else if (argc == 3 && strcmp(op, "car") == 0) {
+		char* a = argv[2];
+		r = _car(atof(a));
 		printf("%lf",r);
 	}
-	else if (strcmp(op, "mul") == 0) {
-		r = _mul(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "div") == 0) {
-		r = _div(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else {printf("Erreur de parametres");}
+	else {
+		printf("Erreur de parametres");
 	}
 	else {printf("Erreur de parametres");}
 	return 0;
